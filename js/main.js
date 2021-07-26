4c36251bc9913e3641db0a00cc9ef5b65f145db0
Victor Bucci <vr.bucci@gmail.com>
2021-07-26 17:26:32 -0300

    small changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert("Olá Mundo do arquivo JavaScript separado!!");
+
+var nome = "Magrão";
+alert("Dá o papo " + nome);
+
+var pinga = 51;
+var furunfa = 69;
+alert(pinga + furunfa);
+
+var frase = "Afeganistão é um excelente lugar para passar férias."
+console.log(frase.replace("Afeganistão", "Polônia"))
+console.log(frase.toUpperCase())
